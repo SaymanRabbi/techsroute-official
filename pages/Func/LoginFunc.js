ff3db225a1f1385983dd0e57c9ce1226c27b4acb
saymanrabbi <mdrabbi.asm@gmail.com>
2023-02-12 11:22:57 +0600

    remove login func

--- pages/Func/LoginFunc.js
+++
@@ -1,8 +0,0 @@
-import pb from "../Lib/pocketbase";
-
-const LoginFunc =async (email,password) => {
-    const authData = await pb.collection('users').authWithPassword(email,password);
-    return authData;
-}
-
-export default LoginFunc
