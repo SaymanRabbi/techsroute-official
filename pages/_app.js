8d0a0b6f6a85679db06beedcab600c8feb9b53b4
saymanrabbi <mdrabbi.asm@gmail.com>
2023-02-12 12:11:42 +0600

    remove store

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,11 @@ import '@/styles/globals.css'
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import { Provider } from 'react-redux'
-import store from '../src/app/store'
+// import store from '../src/app/store'
+// store={store}
 config.autoAddCss = false
 export default function App({ Component, pageProps }) {
-  return <Provider store={store}>
+  return <Provider >
     <Component {...pageProps} />
   </Provider>
 }
